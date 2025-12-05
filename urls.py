826bf990feb36f91da7a903d32cdb33930574d41
ganfonchik <nikito0sick@gmail.com>
2025-12-06 01:38:38 +0500

    почти конец

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
 import ai_brain
+import ai_compare
 import universities_views
 
 urlpatterns = [
@@ -9,6 +10,8 @@ urlpatterns = [
     path('test/', TemplateView.as_view(template_name='test.html'), name='test'),
     path('universities/', universities_views.universities_list, name='universities'),
     path('university/<str:slug>/', universities_views.university_detail, name='university_detail'),
+    path('compare/', TemplateView.as_view(template_name='compare.html'), name='compare'),
     path('admin/', admin.site.urls),
     path('ai-chat/', ai_brain.ai_chat, name='ai_chat'),
+    path('ai-compare/', ai_compare.ai_compare, name='ai_compare'),
 ]
